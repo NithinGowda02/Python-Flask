5a6386636d7a44266ee8b97a938fa3689bf54d5a
Nithin K P <nithinkp0212@gmail.com>
2026-04-21 18:56:45 +0530

    Added Simple Login Page

--- login_page/app.py
+++ login_page/app.py
@@ -1,42 +1,36 @@
-from flask import Flask, redirect, request, Response, url_for, session
+from flask import Flask, redirect, request, url_for, session, render_template
 
 app = Flask(__name__)
 app.secret_key = "supersecret"
 
 @app.route("/", methods=["GET", "POST"])
 def login():
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
+    return render_template("login.html")
 
-        if username == "admin" and password == "123":
-            session["user"] = username
-            return redirect(url_for("welcome"))
-        else:
-            return "Invalid credentials"
 
-    return '''
-        <h2>Login Page</h2>
-        <form method="POST">
-            USERNAME : <input type="text" name="username"><br><br>
-            PASSWORD : <input type="password" name="password"><br><br>
-            <input type="submit" value="Login">
-        </form>
-    '''
+@app.route("/submit", methods=["POST"])
+def submit():
+    username = request.form.get("username").strip()
+    password = request.form.get("password").strip()  # DEBUG
+
+    if username == "admin" and password == "123":
+        return redirect(url_for("welcome"))
+    else:
+        return "Invalid credentials"
+
 
 @app.route("/welcome")
 def welcome():
     if "user" in session:
-        return f'''
-            <h2>WELCOME {session["user"]}!</h2>
-            <a href="/logout">Logout</a>
-        '''
-    return redirect(url_for("login"))
+        return render_template("welcome.html", name=session["user"])
+    else:
+        return redirect(url_for("login.html"))
+
 
 @app.route("/logout")
 def logout():
-    session.pop("user", None)
-    return redirect(url_for("login"))
+    return redirect(url_for("login.html"))
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
